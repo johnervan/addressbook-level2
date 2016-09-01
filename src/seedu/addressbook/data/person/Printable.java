419946294a1371b3dba79f3255cc4f424489f2c1
johnervan <johnervan@u.nus.edu>
2016-09-01 21:37:23 +0800

    Add interface Printable

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package seedu.addressbook.data.person;
+
+/**
+ * @author Johnervan
+ * Interface to produce a printable string representation of a Name, Phone, Email
+ * and/or Address object
+ */
+public interface Printable {
+	
+	public String getPrintableString(); //Returns a string representation of the object
+
+}
